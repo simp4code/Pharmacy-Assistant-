a5b2f584a0979e7f280a01582dc4f82ed4e3777e
Karim Paul Lomonsod <95675914+simp4code@users.noreply.github.com>
2022-04-21 16:13:25 +0800

    Add files via upload

---
+++ src/DatabaseConnection.java
@@ -0,0 +1,15 @@
+import java.sql.*;
+public class DatabaseConnection {
+    public static void main(String args [] ){
+    
+//        try{
+//        
+//            Class.forName("com.mysql.jdbc");
+//            
+//            
+//        }catch(SQLException e ){
+//          e.printStackTrace();
+//        }
+    
+    }
+}
